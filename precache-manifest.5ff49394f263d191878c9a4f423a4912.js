19cf458265ca738863eb9c61ad5ee4d2d7aa4855
Uros Jevtic <uros.jevta@gmail.com>
2020-07-22 23:34:18 +0200

    Updates

---
+++ precache-manifest.5ff49394f263d191878c9a4f423a4912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e931712520a773fcd96045b1760add00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91cd218f5c157e13c626",
+    "url": "/monsters-rolodex/static/css/main.3e217d11.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91cd218f5c157e13c626",
+    "url": "/monsters-rolodex/static/js/main.83b2fe25.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
